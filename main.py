aaaa4ece61645aa9b2c8060a5e1b15b09e6bf421
Shrivara <shrivara.jois@gmail.com>
2017-11-03 13:41:41 +0530

    Clean up reqExec

--- main.py
+++ main.py
@@ -1,10 +1,2 @@
-import os
-from execute import Execute
-
 if __name__ == '__main__':
-    print('Booting up reqExec')
-    executor = Execute()
-    print('Running reqExec script')
-    exit_code=executor.run()
-    print('reqExec has completed')
-    os._exit(exit_code)
+    print "Booting up reqExec..."
