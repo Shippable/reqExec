13616c14f525b02812333a816f0851bbed86756a
Niranjan Godoble <niranjan8192@gmail.com>
2017-11-28 14:14:02 +0000

    remove newline

--- package/x86_64/WindowsServer_2016/package.ps1
+++ package/x86_64/WindowsServer_2016/package.ps1
@@ -5,7 +5,6 @@ function buildReqExec
   pip install pyinstaller==3.3
   pip install -r requirements.txt
   python -m PyInstaller --clean --hidden-import=requests -F main.py
-
 }
 
 # TODO: move this to pipelines later
