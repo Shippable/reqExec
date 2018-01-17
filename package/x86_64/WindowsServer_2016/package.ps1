b51c6a24cc1da990a1831fb093f1010fad354d95
Vishnu Bharathi <vishnubharathi04@gmail.com>
2018-01-17 12:48:39 +0530

    removes packageBinaries

--- package/x86_64/WindowsServer_2016/package.ps1
+++ package/x86_64/WindowsServer_2016/package.ps1
@@ -7,20 +7,5 @@ function buildReqExec
   python -m PyInstaller --clean --hidden-import=requests -F main.py
 }
 
-# TODO: move this to pipelines later
-function packageBinaries
-{
-  $bin_directory = ".\dist\main"
-  New-Item -ItemType Directory -Force -Path $bin_directory
-  Move-Item -force .\dist\main.exe $bin_directory
-  $tmpReqExecPath = "$Env:USERPROFILE/shippable/tmp/reqExec"
-  New-Item -ItemType Directory -Force -Path $tmpReqExecPath
-  Copy-Item -Force -Recurse dist $tmpReqExecPath/
-  tar -zcvf reqExec.tar.gz -C $tmpReqExecPath .
-}
-
 echo "building reqExec binaries"
 buildReqExec
-
-echo "compressing binaries"
-packageBinaries
